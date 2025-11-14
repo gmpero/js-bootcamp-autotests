863ff8464cd73442160fe029adbc76d98788a2cd
Anastasiya <anastasiyazenevich@gmail.com>
2025-11-14 22:12:44 +0300

    firstCommit

---
+++ tests/copyExample.spec.js
@@ -0,0 +1,14 @@
+import { expect } from "@playwright/test";
+import { test } from "../base.js";
+
+test.describe("Verifycation elements on Home Page", () => {
+	test("page title", async ({ page }) => {
+		// Expect a title
+		await expect(page).toHaveTitle("Dashboard - Jenkins");
+	});
+
+	test("page header", async ({ page }) => {
+		// Expect a header
+		await expect(page.locator("#jenkins-head-icon")).toBeVisible();
+	});
+});
