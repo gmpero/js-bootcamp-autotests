44dc887e84abf86e35699c664088535de9f9c969
gmpero <72013308+gmpero@users.noreply.github.com>
2025-11-23 00:05:53 +0300

    RF_US_02.001 | Freestyle Project Configuration > Enable or Disable the Project (#44)

---
+++ helpers/US_02.001.helper.js
@@ -0,0 +1,24 @@
+import { TestData } from "../tests/testData/US_02.001.testData";
+
+export class US_02_001_Helper {
+    static async createFreestyleProject(page) {
+        await page.locator(".task-link-text").first().click();
+        await page.locator("#name").fill(TestData.project.displayName);
+        await page.locator(".hudson_model_FreeStyleProject").click();
+        await page.locator("#ok-button").click();
+        await page.locator("#jenkins-head-icon").click();
+        await page.waitForLoadState("load");
+    }
+
+    static async openProjectDropdown(page) {
+        await page.getByRole('link', { name: `${TestData.project.displayName}`, exact: true }).hover();
+        await page.locator(".jenkins-menu-dropdown-chevron").click();
+    }
+
+    static async openConfigurateProject(page) {
+        await page.getByRole('link', { name: `${TestData.project.displayName}`, exact: true }).focus();
+        await page.keyboard.press('Enter');
+        await page.waitForURL(`/job/${TestData.project.displayName}/`)
+        await page.getByRole("link", {name: `${TestData.dropdown.items[3]}`}).click();
+    }
+}
