8e23078eadc8eae963966509ffc7a676933bef99
Rostislav <penshinrost@gmail.com>
2025-12-13 14:53:58 +0300

    TC_01.001.05 | Verify that home page displays correct start building title

--- POM/testData/homePage.js
+++ POM/testData/homePage.js
@@ -1,6 +1,22 @@
+import { title } from "process";
+
 const expectHomePage = {
     welcomeTitle : "Welcome to Jenkins!",
     descriptionForWelcomeTitle: "This page is where your Jenkins jobs will be displayed. To get started, you can set up distributed builds or start building a software project.",
+
+    startBuildingSection : {
+        title: "Start building your software project",
+        createJobName: "Create a job",
+    },
+
+    distributedBuildSection : {
+        title: "Set up a distributed build",
+        listDistributedBuildSectionLink : {
+            setUpAnAgent : "Set up an agent",
+            configureCloud : "Configure a cloud",
+            learnMore : "Learn more about distributed builds",
+        }
+    },
 };
 
 export default expectHomePage;
