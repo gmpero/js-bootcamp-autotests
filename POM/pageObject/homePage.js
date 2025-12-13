507909f58658add750baea64c09aad3eeb157888
Rostislav <penshinrost@gmail.com>
2025-12-13 15:52:40 +0300

    Verify distributed build section links

--- POM/pageObject/homePage.js
+++ POM/pageObject/homePage.js
@@ -17,6 +17,7 @@ class HomePage extends Layout {
     getLocatorStartBuildingTitle = () => this.page.locator(".empty-state-section h2.h4").first();
     getLocatorCreateJobLink = () => this.page.locator(".empty-state-section-list .content-block .content-block__link").first();
     getLocatorDistributedBuildTitle = () => this.page.locator(".empty-state-section h2.h4").nth(1);
+    getLocatorDistributedBuildLinks = () => this.page.locator(".empty-state-section-list").nth(1).locator('.content-block__link');;
 
     async clickNewItem() {
         await this.getLocatorNewItem().click();
