dcdb92bd6f992ea952e3ca818b1fd9fc4815b50f
Rostislav <penshinrost@gmail.com>
2025-12-14 12:31:41 +0300

    RF : reorganize home page test structure

--- POM/tests/homePage.spec.js
+++ POM/tests/homePage.spec.js
@@ -8,12 +8,7 @@ import globalData from "../testData/globalData.js";
 
 
 test.describe("US_01.001 | New Item > Create a new item", () => {
-	test("RF_01.001.01 | Verify the button 'New Item' is visible", async ({ page }) => {
-		const homePage = new HomePage(page); 
-        await expect(homePage.getLocatorNewItem()).toBeVisible();
-	});
-
-    test("RF_01.001.02 | Verify new project creation", async ({ page }) => {
+    test("TC_01.001.01 | Verify new project creation", async ({ page }) => {
         const homePage = new HomePage(page);
         const newItemPage = await homePage.clickNewItem();
         await newItemPage.fillJenkinsInput(globalData.projectFreestyleName);
@@ -23,43 +18,50 @@ test.describe("US_01.001 | New Item > Create a new item", () => {
 
         expect(homePage.getLocatorItemName()).toContainText(globalData.projectFreestyleName);
     });
+    
+    // 5. Вынести футер в layout
+    // 6. Написание тестов на футер.
+});
 
-    test("TC_01.001.03 | Verify that home page displays correct welcome title", async ({page}) => {
+test.describe("US_11.001 | Welcome Dashboard > View Welcome Message and start page", () => {
+    test("TC_01.001.01 | Verify the button 'New Item' is visible", async ({ page }) => {
+		const homePage = new HomePage(page); 
+        await expect(homePage.getLocatorNewItem()).toBeVisible();
+	});
+
+    test("TC_01.001.02 | Verify that home page displays correct welcome title", async ({page}) => {
         const homePage = new HomePage(page);
         expect(homePage.getLocatorWelcomeTitle()).toHaveText(expectHomePage.welcomeTitle);
     });
 
-    test("TC_01.001.04 | Verify that home page displays correct welcome description", async ({page}) => {
+    test("TC_01.001.03 | Verify that home page displays correct welcome description", async ({page}) => {
         const homePage = new HomePage(page);
         expect(homePage.getLocatorDescriptionForWelcomeTitle()).toHaveText(expectHomePage.descriptionForWelcomeTitle);
     });
 
-    test("TC_01.001.05 | Verify that home page displays correct start building title", async ({page}) => {
+    test("TC_01.001.04 | Verify that home page displays correct start building title", async ({page}) => {
         const homePage = new HomePage(page);
         expect(homePage.getLocatorStartBuildingTitle()).toHaveText(expectHomePage.startBuildingSection.title);
     });
 
-    test("TC_01.001.06 | Verify that home page displays correct 'Create Job Name' link", async ({page}) => {
+    test("TC_01.001.05 | Verify that home page displays correct 'Create Job Name' link", async ({page}) => {
         const homePage = new HomePage(page);
         expect(homePage.getLocatorCreateJobLink()).toHaveText(expectHomePage.startBuildingSection.createJobNameLink);
     });
 
-    test("TC_01.001.07 | Verify that 'Create Job Name' link navigates to New Item page", async ({page}) => {
+    test("TC_01.001.06 | Verify that 'Create Job Name' link navigates to New Item page", async ({page}) => {
         const homePage = new HomePage(page);
         homePage.clickCreateJobLink();
         await expect(page).toHaveURL(expectNewItemPage.url);
     });
 
-    test("TC_01.001.08 | Verify that home page displays correct distributed build section title", async ({page}) => {
+    test("TC_01.001.07 | Verify that home page displays correct distributed build section title", async ({page}) => {
         const homePage = new HomePage(page);
         expect(homePage.getLocatorDistributedBuildTitle()).toHaveText(expectHomePage.distributedBuildSection.title);
     });
 
-    test("TC_01.001.9 | Verify distributed build section links", async ({page}) => {
+    test("TC_01.001.08 | Verify distributed build section links", async ({page}) => {
         const homePage = new HomePage(page);
         expect(homePage.getLocatorDistributedBuildLinks()).toHaveText(expectHomePage.distributedBuildSection.getListNameDistributedBuildSectionLink());
     });
-    
-    // 5. Вынести футер в layout
-    // 6. Написание тестов на футер.
 });
